42f58d5b72b2ea8652aa9b01e3bd359942735701
CollinsDrew <collinsdrew1990@gmail.com>
2023-03-16 21:19:30 +0000

    hero comp added and card comp added however stock images seem to have styled of but real images not looking right

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import TopBar from "./components/TopBar";
+import Cards from "./components/Cards";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
       <Navbar />
+      <Hero />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import bigJelly from "../Assets/bigjelly.jpeg";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-[90vh] ">
+      <img
+        src={bigJelly}
+        alt="bigJelly"
+        className="w-full h-full object-cover"
+      />
+
+      <div className="m-w-[1140px] m-auto">
+        <div className="absolute top-[40%] w-full md:[50%] max-w-600px h-full flex flex-col text-white p-4">
+          <h1 className="font-bold text-4xl">Welcome to</h1>
+          <h2 className="text-4xl py-4 italic">Big Jelly Studios</h2>
+          <p>
+            Based in a beautiful hidden chapel in the heart of seaside town
+            Ramsgate.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
