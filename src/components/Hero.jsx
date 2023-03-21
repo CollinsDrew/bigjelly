da3e8b2f4e323429d2dc464171814e699bb12875
CollinsDrew <collinsdrew1990@gmail.com>
2023-03-21 21:30:55 +0000

    small style changes to text in hero comp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,9 +12,22 @@ const Hero = () => {
 
       <div className="m-w-[1140px] m-auto">
         <div className="absolute top-[40%] w-full md:[50%] max-w-600px h-full flex flex-col text-white p-4">
-          <h1 className="font-bold text-4xl">Welcome to</h1>
-          <h2 className="text-4xl py-4 italic">Big Jelly Studios</h2>
-          <p>
+          <h1
+            className="font-bold text-4xl text-shadow-lg"
+            style={{ textShadow: "0px 3px 3px rgba(0,0,0,0.3)" }}
+          >
+            Welcome to
+          </h1>
+          <h2
+            className="text-4xl py-4 italic text-shadow-lg"
+            style={{ textShadow: "0px 3px 3px rgba(0,0,0,0.3)" }}
+          >
+            Big Jelly Studios
+          </h2>
+          <p
+            className="text-shadow-lg"
+            style={{ textShadow: "0px 3px 3px rgba(0,0,0,0.3)" }}
+          >
             Based in a beautiful hidden chapel in the heart of seaside town
             Ramsgate.
           </p>
